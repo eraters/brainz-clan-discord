416f8771c5c5a4fbcc29574e547522db0fcaaf2b
imlazywithazismyname <54771596+imlazywithazismyname@users.noreply.github.com>
2019-11-07 08:05:18 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands, tasks
+
+client = commands.Bot(command_prefix = ',')
+
+@client.event
+async def on_ready():
+  print('Bot is ready.')
+  
+@client.command()
+async def hello(ctx):
+  print(f'{ctx.author} says hello! (The guild id is {ctx.guild.id} and the guild name is {ctx.guild})!')
+
+client.run(get_env('TOKEN'))
